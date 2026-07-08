6901fa767e36e83a9fba2e17067e018baff12302
vikramkher <vickru.kher@gmail.com>
2026-07-08 13:55:19 +0530

    ok'

--- Exception/src/exception/InsufficentFundException.java
+++ Exception/src/exception/InsufficentFundException.java
@@ -1,6 +1,11 @@
 package exception;
 
 public class InsufficentFundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public InsufficentFundException() {
 		super("insufficent fund trasfer");
 	}
--- Exception/src/exception/LoginException.java
+++ Exception/src/exception/LoginException.java
@@ -2,6 +2,11 @@ package exception;
 
 public class LoginException extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1954246253432570683L;
+
 	public LoginException() {
 		super("Invalid login and password");
 	}
